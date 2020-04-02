d6f0c37d93fe37c36bb63af6508f0ae52a8e1b39
guopeng1129972 <381694179@qq.com>
2020-04-02 23:07:59 +0800

    ts+react

--- 3/readme.md
+++ 3/readme.md
@@ -1,2 +1,17 @@
 react
 
+yarn install
+yarn create react-app react-starter
+//没试
+yarn create react-app ts-with-react --typescript
+npx 安装
+npx create-react-app ts-with-react --typescript
+npx 安装的优点
+1.避免安装全局模块
+2.调用项目内部安装的模块
+
+
+hook的特点
+1.完全可选
+2.百分之百向后兼容（会不会class 不影响使用不适用hook）
+3.没有计划移除react中的class
---
+++ 3/ts-with-react/src/componets/hello.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// 使用interface的优点
+//1.传出到app的时候，会验证属性
+//2.可以获得代码提示
+interface myHelloProps{
+  message?:string;
+}
+//可以使用react定义的泛型
+// const Hello:React.FunctionComponent<myHelloProps> = (props) => {
+//类型别名
+// type FC<P = {}> = FunctionComponent<P>;
+//拓展方法children defaultProps
+const Hello:React.FC<myHelloProps> = (props) => {
+  // props.children  
+  return <h2>{props.message}</h2>
+
+}
+//defaultProps 定义默认值，
+Hello.defaultProps={
+  message:'hello World@！！'
+}
+
+export default Hello;
