8ba4938fbf385505bbe48610973d20922b4c6974
guopeng1129972 <381694179@qq.com>
2020-04-24 22:26:09 +0800

    alert就先写到这吧
    alert 关闭的事件没哟完成，也不会了，就先到这，写了个弹框先放哪，往后面走吧

--- 4/kunkunball/src/components/Alert/alert.tsx
+++ 4/kunkunball/src/components/Alert/alert.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classNames from 'classnames';
 
 export enum altSty {
@@ -21,17 +21,18 @@ interface baseAlertProps {
 
   children?: React.ReactNode;
 }
-
-const Alert: React.FC<baseAlertProps> = (props) => {
+type NativeAlertProps = baseAlertProps & React.AreaHTMLAttributes<HTMLElement>;
+type AnchorAlertProps = baseAlertProps & React.AnchorHTMLAttributes<HTMLElement>;
+export type AlertProps = Partial<NativeAlertProps & AnchorAlertProps>;
+const Alert: React.FC<AlertProps> = (props) => {
   const {
     className,
     altType,
-    // altState,
     altStyle,
     children,
     title,
-    ...resProps
   } = props
+  const domRef = useRef<HTMLDocument>(null)
 
   const classes = classNames('alt', className, {
     [`alt-${altType}`]: altType,
@@ -39,15 +40,27 @@ const Alert: React.FC<baseAlertProps> = (props) => {
 
     // altState: (altType !== alertType.Warning) && altState
   })
+  const onClose = () => {
+    alert(1)
+  }
 
   if (altStyle === 'spe') {
     return (
       <div
         className={classes}
       >
-        <span
+        <span>
+          {title}
+        </span>
+        <p
           className={'alt-spe-span'}
-        >{children}</span>
+        >{children}</p>
+        <span
+          className={'alt-no-clase-span'}
+          onClick={onClose}>
+
+          X
+        </span>
       </div>
     )
   } else {
@@ -57,9 +70,11 @@ const Alert: React.FC<baseAlertProps> = (props) => {
       >
         <span
           className={'alt-no-span'}
-        >{children}</span>
+        >{children}
+        </span>
         <span
-        className={'alt-no-clase-span'}>
+          onClick={onClose}
+          className={'alt-no-clase-span'}>
           X
         </span>
       </div>
